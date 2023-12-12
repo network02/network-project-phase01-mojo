375c75addf8e7e4b23941997f46e29f99a3e7bbc
muhuchah <m.h.chah4@gmail.com>
2023-12-12 20:03:05 +0330

    add is_online for server and is_open for ports

--- nmap.py
+++ nmap.py
@@ -10,7 +10,32 @@ class nmap:
         self.HOST_IP = IP
         self.HEADERSIZE = headersize
 
+    def is_online(self):
+        try:
+            socket.gethostbyaddr(self.HOST_IP)
+        except socket.error:
+            return False
+        return True
+
+    def is_open(self, port_num):
+        try:
+            self.s.connect((self.HOST_IP, port_num))
+        except socket.error:
+            return False
+        
+        return True
+
 
 host_ip = sys.argv[1]
 
 nm = nmap(IP=host_ip, headersize=10)
+
+if nm.is_online():
+    print(f"{host_ip} is online.")
+else:
+    print(f"{host_ip} is offline.")
+
+check = sys.argv[2]
+
+fport = int(sys.argv[2])
+eport = int(sys.argv[3])
